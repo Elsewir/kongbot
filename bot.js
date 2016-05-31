8d0b5408f239e9bee19ad4582a03be630d8c8815
Toprak Öztürk <toprakozt@gmail.com>
2016-06-01 00:09:00 +0300

    Bir iki hatayı düzelttim, koda şekil düzen verdim.

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        ElseBOT
+// @name        KafirBOT
 // @namespace   kong
 // @include     http://www.kongregate.com/games/*
 // @version     1
@@ -10,23 +10,24 @@ var version = '1.1.1'; // Versiyon numarası.
 var afk = false; // Başladığında çalışır halde olacak.
 var time;
 
- 
 var responseList = [
     // MUHAMMED
     ['muhammed', '(SAV).'],
     ['muhammet', '(SAV).'],
+
     // Komutlar
     ['!komutlar', '!bilgi,else,!anan,!kürt,!sözler,'],
     ['!bilgi', 'ElseBOT versiyon 31.2.1'],
     ['!sözler', '!mehmet akif,!mevlana,!nazım hikmet,!necip fazıl'],
     ['else', 'Buyrun benim'],
     ['!anan', 'https://i.ytimg.com/vi/Tb2FcQ0qaPY/maxresdefault.jpg'],  
-    ['!kürt', 'https://c11.incisozluk.com.tr/res/incisozluk/11006/1/199381_o66f9.jpg'],  
+    ['!kürt', 'https://c11.incisozluk.com.tr/res/incisozluk/11006/1/199381_o66f9.jpg'], 
+
     //SÖZLER 
-   ['!mehmet akif', 'Ya rab, bu uğursuz gecenin yok mu sabahı? Mahşerde mi biçarelerin, yoksa felahı?'],
-   ['!mevlana', 'Irmak suyunu tümden içmenin imkanı yok ama susuzluğu giderecek kadar içmemenin de imkanı yok.'],
-   ['!nazım hikmet', 'Ya ölü yıldızlara götüreceğiz hayatı, ya da ölüm inecek yeryüzüne.'], 
-   ['!necip fazıl', 'Dünya öküzün üstünde derler ama,ben dünya üzerinde nice öküzler bilrim.'], 
+    ['!mehmet akif', 'Ya rab, bu uğursuz gecenin yok mu sabahı? Mahşerde mi biçarelerin, yoksa felahı?'],
+    ['!mevlana', 'Irmak suyunu tümden içmenin imkanı yok ama susuzluğu giderecek kadar içmemenin de imkanı yok.'],
+    ['!nazım hikmet', 'Ya ölü yıldızlara götüreceğiz hayatı, ya da ölüm inecek yeryüzüne.'], 
+    ['!necip fazıl', 'Dünya öküzün üstünde derler ama,ben dünya üzerinde nice öküzler bilrim.'], 
 
     // KÜFÜRLER
     ['göt', 'Küfür etme adam ol.'],
@@ -53,14 +54,14 @@ var responseList = [
     ['tanrı', '(C.C)'],
     ['Tanrı', '(C.C)'],
     ['tekbir', 'ALLAHU EKBER'],
-    
-
 ];
 
+// Bot'un mesajlarına cevap verdiği kişiler
 var whiteList = [
-    "discontinue","seksek1","sterlars","sahin9999","omeragasxex","AlpKaanB","exandtrix09","yagtinmer","fatihkutay","YusufO48","MetehanO24"
+    "discontinue", "seksek1", "sterlars", "sahin9999", "omeragasxex", "AlpKaanB", "exandtrix09", "yagtinmer", "fatihkutay", "YusufO48", "MetehanO24"
 ];
 
+// Opsiyonel özellikler için ses dosyası eklendi.
 var audio = document.createElement('audio'); // Ses dosyasını yarat.
 audio.volume = 0.50;
 audio.setAttribute('src', 'http://www.soundjay.com/button/sounds/beep-01a.mp3');
@@ -75,66 +76,56 @@ window.konduitToHolodeck = function (a) {
     var message = [];
     var error = false;
 
+    // Alınan veriyi konsola yazdır (Tarayıcıdayken F12 basıp görüntüleniyor)
     console.log(parsed.data);
+
     try {
         try {
             username = parsed.data.user.username;
         } catch (e) {
             username = parsed.data.from;
-        }
+    }
         
-        
-        
-
-        if (whiteList.indexOf(username) > -1)
-            error = true;
-           
-            
-        if ((parsed.data.message.indexOf('else') > -1) && !error)
-            audio.play();
-        
-        
-        if (parsed.data.message.indexOf('kapanın ben imamım') > -1)
-            afk = !afk;
-        
-        if (username == parsed.data.from) error = true; 
+    if (whiteList.indexOf(username) > -1)
+        error = true;
        
-        //containsWord(parsed.data.message, responseList[i][0]
-        for (var i = 0; i < responseList.length; i++) {
-            if (parsed.data.message == responseList[i][0] && !error) {
-                message.push(responseList[i][1] + "\n");
-            }
+    if (parsed.data.message.indexOf('kapanın ben imamım') > -1)
+        afk = !afk;
+    
+    if (username == parsed.data.from) error = true; 
+   
+    //containsWord(parsed.data.message, responseList[i][0]
+    for (var i = 0; i < responseList.length; i++) {
+        if (parsed.data.message == responseList[i][0] && !error) {
+            message.push(responseList[i][1] + "\n");
         }
+    }
 
-        if (message.length === 0)
-            error = true;
+    if (message.length === 0)
+        error = true;
 
-  if (!error && !afk) {     // && mods(parsed.data.user.username) == true
-      for (var i = 0; i < message.length; i++) {
-        console.log(message[i]);          
-        document.getElementById('konduit').dispatchJSONEvent(
-        JSON.stringify({
-            "type": "room_message",
-            "data": {
-                "message": message[i],
-                "room": {
-                    "id": "15",
-                    "type": "chat",
-                    "name": "Şu Çılgın Türkler",
-                    "xmpp_name": "15",
-                    "owner": "Tarantulka"
-                }
-            }
-        }));   
-      }
+    if (!error && !afk) {     // && mods(parsed.data.user.username) == true
+        for (var i = 0; i < message.length; i++) {
+            console.log(message[i]);          
+            document.getElementById('konduit').dispatchJSONEvent(
+                JSON.stringify(
+                {
+                    "type": "room_message",
+                    "data": {
+                        "message": message[i],
+                        "room": 
+                        {
+                            "id": "15",
+                            "type": "chat",
+                            "name": "Şu Çılgın Türkler",
+                            "xmpp_name": "15",
+                            "owner": "Tarantulka"
+                        }
+                    }
+                })
+            );   
+        }
     }
   } catch (c) { }
   old(a);
 };
-
-/*
-holodeck._chat_commands.cls = function (deck, text) {
-  holodeck.activeDialogue().clear();
-  return false
-};
-*/
